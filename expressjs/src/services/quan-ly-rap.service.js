8c3c8a6a984563edb6a573e3fedc30341a3b156a
lazy128 <duy.nguyenduy128@hcmut.edu.vn>
2026-05-30 18:29:51 +0700

    Fix LayThongTinLichChieu response payload format to match FE expectation

--- expressjs/src/services/quan-ly-rap.service.js
+++ expressjs/src/services/quan-ly-rap.service.js
@@ -27,7 +27,7 @@ export const quanLyRapService = {
     if (maHeThongRap) {
       where.ma_he_thong_rap = maHeThongRap;
     }
-    const result = await prisma.he_thong_rap.findMany({
+    const heThongRaps = await prisma.he_thong_rap.findMany({
       where,
       include: {
         cum_rap: {
@@ -43,7 +43,30 @@ export const quanLyRapService = {
         }
       }
     });
-    return result;
+
+    return heThongRaps.map(htr => ({
+      maHeThongRap: htr.ma_he_thong_rap,
+      tenHeThongRap: htr.ten_he_thong_rap,
+      logo: htr.logo,
+      cumRapChieu: htr.cum_rap.map(cum => ({
+        maCumRap: cum.ma_cum_rap,
+        tenCumRap: cum.ten_cum_rap,
+        diaChi: cum.dia_chi,
+        lichChieuPhim: cum.rap_phim.map(rap => ({
+          maRap: rap.ma_rap,
+          tenRap: rap.ten_rap,
+          lichChieu: rap.lich_chieu.map(lc => ({
+            maLichChieu: lc.ma_lich_chieu,
+            maRap: lc.ma_rap,
+            maPhim: lc.ma_phim,
+            ngayGioChieu: lc.ngay_gio_chieu,
+            giaVe: lc.gia_ve,
+            tenPhim: lc.phim.ten_phim,
+            hinhAnh: lc.phim.hinh_anh
+          }))
+        })).filter(rap => rap.lichChieu.length > 0)
+      })).filter(cum => cum.lichChieuPhim.length > 0)
+    }));
   },
 
   async LayThongTinLichChieuPhim(req) {
@@ -60,6 +83,80 @@ export const quanLyRapService = {
         }
       }
     });
-    return phim;
+
+    if (!phim) return null;
+
+    const heThongRapMap = {};
+
+    phim.lich_chieu.forEach(lc => {
+      const rap = lc.rap_phim;
+      const cum = rap.cum_rap;
+      const htr = cum.he_thong_rap;
+
+      if (!heThongRapMap[htr.ma_he_thong_rap]) {
+        heThongRapMap[htr.ma_he_thong_rap] = {
+          maHeThongRap: htr.ma_he_thong_rap,
+          tenHeThongRap: htr.ten_he_thong_rap,
+          logo: htr.logo,
+          cumRapChieuMap: {}
+        };
+      }
+
+      const htrMap = heThongRapMap[htr.ma_he_thong_rap].cumRapChieuMap;
+
+      if (!htrMap[cum.ma_cum_rap]) {
+        htrMap[cum.ma_cum_rap] = {
+          maCumRap: cum.ma_cum_rap,
+          tenCumRap: cum.ten_cum_rap,
+          diaChi: cum.dia_chi,
+          lichChieuPhimMap: {}
+        };
+      }
+
+      const crMap = htrMap[cum.ma_cum_rap].lichChieuPhimMap;
+
+      if (!crMap[rap.ma_rap]) {
+        crMap[rap.ma_rap] = {
+          maRap: rap.ma_rap,
+          tenRap: rap.ten_rap,
+          lichChieu: []
+        };
+      }
+
+      crMap[rap.ma_rap].lichChieu.push({
+        maLichChieu: lc.ma_lich_chieu,
+        maRap: lc.ma_rap,
+        maPhim: lc.ma_phim,
+        ngayGioChieu: lc.ngay_gio_chieu,
+        giaVe: lc.gia_ve
+      });
+    });
+
+    const heThongRapChieu = Object.values(heThongRapMap).map(htr => ({
+      maHeThongRap: htr.maHeThongRap,
+      tenHeThongRap: htr.tenHeThongRap,
+      logo: htr.logo,
+      cumRapChieu: Object.values(htr.cumRapChieuMap).map(cum => ({
+        maCumRap: cum.maCumRap,
+        tenCumRap: cum.tenCumRap,
+        diaChi: cum.diaChi,
+        lichChieuPhim: Object.values(cum.lichChieuPhimMap)
+      }))
+    }));
+
+    return {
+      maPhim: phim.ma_phim,
+      tenPhim: phim.ten_phim,
+      biDanh: phim.bi_danh,
+      trailer: phim.trailer,
+      hinhAnh: phim.hinh_anh,
+      moTa: phim.mo_ta,
+      ngayKhoiChieu: phim.ngay_khoi_chieu,
+      danhGia: phim.danh_gia,
+      hot: phim.hot,
+      dangChieu: phim.dang_chieu,
+      sapChieu: phim.sap_chieu,
+      heThongRapChieu
+    };
   }
 };
